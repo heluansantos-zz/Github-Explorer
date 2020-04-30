249e754d9293bdcf977c86b588103cabf7095732
heluansantos <heluan.caetano@gmail.com>
2020-04-30 19:11:37 -0300

    Lidando com erros

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
-import { Title, Form, Repositories } from './styles';
+import { Title, Form, Repositories, Error } from './styles';
 import api from '../../assets/services/api';
 
 interface Repository {
@@ -12,30 +12,36 @@ interface Repository {
     avatar_url: string;
   };
 }
-
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
   const [repositories, setRepositories] = useState<Repository[]>([]);
 
   async function handleAddRepository(
     event: FormEvent<HTMLFormElement>,
   ): Promise<void> {
     event.preventDefault();
-    //Adição de um novo repositório
-    //Consumir API do github
-    //Salvar novo repositório no estado
-    const response = await api.get(`repos/${newRepo}`);
-    const repository = response.data;
+    if (!newRepo) {
+      setInputError('Digite o autor/nome do repositório!');
+      return;
+    }
+    try {
+      const response = await api.get(`repos/${newRepo}`);
+      const repository = response.data;
 
-    setRepositories([...repositories, repository]);
-    setNewRepo('');
+      setRepositories([...repositories, repository]);
+      setNewRepo('');
+      setInputError('');
+    } catch (err) {
+      setInputError('Erro na busca por esse repositório!');
+    }
   }
 
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       return <Title>Explore repositórios no Github.</Title>
-      <Form onSubmit={handleAddRepository}>
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
         <input
           value={newRepo}
           onChange={(e) => setNewRepo(e.target.value)}
@@ -43,6 +49,7 @@ const Dashboard: React.FC = () => {
         />
         <button type="submit">Pesquisar</button>
       </Form>
+      {inputError && <Error>{inputError}</Error>}
       <Repositories>
         {repositories.map((repository) => (
           <a key={repository.full_name} href="teste">
